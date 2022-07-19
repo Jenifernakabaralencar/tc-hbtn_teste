9f07c861666a45ef22e720c471ac918a1443ae10
Jenifer <jnakabar@ciandt.com>
2022-07-19 15:48:43 -0300

    ajuste task 0

--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -1,14 +1,23 @@
 public class TesteEmail {
 
     public static boolean testar_email_com_arroba() {
-        return Pessoa.emailValid("email_teste@dominio.com.br");
+        boolean emailValid = Pessoa.emailValid("email_teste@dominio.com.br");
+        if (emailValid){
+            return true;
+        }return false;
     }
 
     public static boolean testar_email_sem_arroba() {
-        return Pessoa.emailValid("email_testedominio.com.br");
+        boolean semArroba = Pessoa.emailValid("email_testedominio.com.br");
+        if (!semArroba){
+            return true;
+        }return false;
     }
 
     public static boolean testar_email_mais_50_caracteres() {
-        return Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
+        boolean emailValid = Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
+        if (!emailValid){
+            return true;
+        }return false;
     }
 }
